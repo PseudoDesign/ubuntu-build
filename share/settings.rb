079ff43d8b490f8e51b19c469ea0d4f7d8df487e
Adam <PseudoDesign@users.noreply.github.com>
2018-07-07 00:50:31 -0600

    added config

--- share/settings.rb
+++ share/settings.rb
@@ -1,20 +1,20 @@
 # Variables that need to be defined in a settings file
-LINUX_BRANCH = "toradex_4.9-1.0.x-imx"
-LINUX_REPO = "git://git.toradex.com/linux-toradex.git"
+LINUX_BRANCH = "rel_imx_4.1.15_2.1.0_ga"
+LINUX_REPO = "git://git.freescale.com/imx/linux-imx.git"
 
-UBOOT_BRANCH = "2016.11-toradex"
-UBOOT_REPO = "git://git.toradex.com/u-boot-toradex.git"
+UBOOT_BRANCH = "imx_v2016.03_4.1.15_2.0.0_ga"
+UBOOT_REPO = "git://git.freescale.com/imx/uboot-imx.git"
 
-UBOOT_CONFIG = 'apalis_imx6_defconfig'
-KERNEL_CONFIG = 'apalis_imx6_defconfig'
+UBOOT_CONFIG = 'mx6ull_14x14_evk_defconfig'
+KERNEL_CONFIG = 'imx_v7_defconfig'
 
-DTB_NAME = "imx6q-apalis-eval.dtb"
+DTB_NAME = "imx6ull-14x14-evk.dtb"
 
 ENABLE_ROOT_ACCOUNT = true
 ENABLE_IMX_SERIAL_CONSOLE = true
 ROOT_PASSWORD = "12345"
 
-UBOOT_BINARY_NAME = "u-boot.img"
+UBOOT_BINARY_NAME = "u-boot.imx"
 
 PARTITION_INFO = [
   {
