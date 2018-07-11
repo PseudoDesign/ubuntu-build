66500585704dac06a9257ec6f3d8bec1b5376b25
Adam <PseudoDesign@users.noreply.github.com>
2018-07-10 22:52:31 -0600

    building image, but with reboot issue

--- share/settings.rb
+++ share/settings.rb
@@ -2,13 +2,13 @@
 LINUX_BRANCH = "4.1-2.0.x-imx"
 LINUX_REPO = "https://github.com/Freescale/linux-fslc.git"
 
-IMPORT_KERNEL_DEFCONFIG = "/share/imx_v7_with_fhandle"
+IMPORT_KERNEL_DEFCONFIG = "/share/imx_v7_with_fhandle_defconfig"
 
-UBOOT_BRANCH = "imx_v2016.03_4.1.15_2.0.0_ga"
-UBOOT_REPO = "git://git.freescale.com/imx/uboot-imx.git"
+UBOOT_BRANCH = "2017.09+fslc"
+UBOOT_REPO = "https://github.com/Freescale/u-boot-fslc.git"
 
 UBOOT_CONFIG = 'mx6ull_14x14_evk_defconfig'
-KERNEL_CONFIG = 'imx_v7_defconfig'
+KERNEL_CONFIG = 'imx_v7_with_fhandle_defconfig'
 
 DTB_NAME = "imx6ull-14x14-evk.dtb"
 
@@ -16,7 +16,7 @@ ENABLE_ROOT_ACCOUNT = true
 ENABLE_IMX_SERIAL_CONSOLE = true
 ROOT_PASSWORD = "12345"
 
-UBOOT_BINARY_NAME = "u-boot.imx"
+UBOOT_BINARY_NAME = "u-boot-dtb.imx"
 
 PARTITION_INFO = [
   {
